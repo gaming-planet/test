9231b2f220ebe9f7bb85a014f5d3529fbbd0bdfc
Rui-Jia <ruijiameng123@gmail.com>
2023-01-13 15:50:19 -0500

    ndbe

--- heart.py
+++ heart.py
@@ -7,13 +7,11 @@ CANVAS_HEIGHT = 480
 CANVAS_CENTER_X = CANVAS_WIDTH / 2  
 CANVAS_CENTER_Y = CANVAS_HEIGHT / 2 
 IMAGE_ENLARGE = 11  
-HEART_COLOR = "#00FFFF"  
+HEART_COLOR = "#ADD8E6"  
  
  
 def heart_function(t, shrink_ratio: float = IMAGE_ENLARGE):
-    """
     
-    """
     
     x = 16 * (sin(t) ** 3)
     y = -(13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t))
@@ -31,6 +29,7 @@ def heart_function(t, shrink_ratio: float = IMAGE_ENLARGE):
  
 def scatter_inside(x, y, beta=0.15):
     """
+    cao ni ma
     
     """
     ratio_x = - beta * log(random.random())
